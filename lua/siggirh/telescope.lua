8543f57af9b87b0f3b0982d680cafabea7160292
siggirh <siggi@50skills.com>
2022-09-08 00:12:34 +0000

    Fix fzf sorting

--- lua/siggirh/telescope.lua
+++ lua/siggirh/telescope.lua
@@ -27,10 +27,12 @@ telescope.setup({
     }
   },
   extensions = {
-    fuzzy = true,
-    override_generic_sorter = true,
-    override_file_sorter = true,
-    case_mode = "smart_case"
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case"
+    }
   }
 })
 
